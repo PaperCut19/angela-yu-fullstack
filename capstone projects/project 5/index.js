ded3f17f5b0075ffec95c220467d89ab26b35ef2
Cristian Sanchez-Espinosa <csesanchez04@icloud.com>
2025-07-08 18:27:08 -0400

    add capstone project 5 folder and set up server

---
+++ capstone projects/project 5/index.js
@@ -0,0 +1,14 @@
+import express from "express";
+import pg from "pg";
+import bodyParser from "body-parser";
+
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+    res.render("index.ejs", { message: "Hello World" });
+});
+
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+});
