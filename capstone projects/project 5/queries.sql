5313a5b33a99a1d984afd189d76004ac6ea2a45f
Cristian Sanchez-Espinosa <csesanchez04@icloud.com>
2025-07-10 19:56:55 -0400

    add documentation to queries file

--- capstone projects/project 5/queries.sql
+++ capstone projects/project 5/queries.sql
@@ -1,8 +1,10 @@
+-- CREATE users table --
 CREATE TABLE users (
     id SERIAL PRIMARY KEY,
     name TEXT NOT NULL UNIQUE
 );
 
+-- CREATE books table --
 CREATE TABLE books (
     id SERIAL PRIMARY KEY,
     title TEXT NOT NULL UNIQUE,
@@ -10,6 +12,7 @@ CREATE TABLE books (
     book_cover TEXT NOT NULL
 );
 
+-- CREATE user_book_notes table --
 CREATE TABLE user_book_notes (
     id SERIAL PRIMARY KEY,
     user_id INTEGER NOT NULL REFERENCES users(id) ON DELETE CASCADE,
@@ -18,6 +21,7 @@ CREATE TABLE user_book_notes (
     page_number TEXT
 );
 
+-- CREATE user_book_reviews, one user can only have one review per book --
 CREATE TABLE user_book_reviews (
     user_id INTEGER NOT NULL REFERENCES users(id) ON DELETE CASCADE,
     book_id INTEGER NOT NULL REFERENCES books(id) ON DELETE CASCADE,
