2cf5652e6cf43756fec2be070a6d8a95b25d9c8d
Cristian Sanchez-Espinosa <csesanchez04@icloud.com>
2025-07-10 22:16:54 -0400

    add express middleware to make user input readable

--- capstone projects/project 5/index.js
+++ capstone projects/project 5/index.js
@@ -8,13 +8,16 @@ const port = 3000;
 app.use('/bootstrap', express.static(
     decodeURIComponent(new URL('./node_modules/bootstrap/dist', import.meta.url).pathname)
 ));
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
     res.render("index.ejs", { message: "Hello World" });
 });
 
 app.post("/user", (req, res) => {
-    res.render("userLibrary.ejs");
+    const user = req.body["user"];
+
+    res.render("userLibrary.ejs", { name: user });
 });
 
 app.listen(port, () => {
