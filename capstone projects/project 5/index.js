fe5d11c2e097c7f3078a53f9ab175c5d432727a6
Cristian Sanchez-Espinosa <csesanchez04@icloud.com>
2025-07-10 21:49:00 -0400

    add userLibrary.ejs file

--- capstone projects/project 5/index.js
+++ capstone projects/project 5/index.js
@@ -5,8 +5,16 @@ import bodyParser from "body-parser";
 const app = express();
 const port = 3000;
 
+app.use('/bootstrap', express.static(
+    decodeURIComponent(new URL('./node_modules/bootstrap/dist', import.meta.url).pathname)
+));
+
 app.get("/", (req, res) => {
     res.render("index.ejs", { message: "Hello World" });
+});
+
+app.post("/user", (req, res) => {
+    res.render("userLibrary.ejs");
 });
 
 app.listen(port, () => {
