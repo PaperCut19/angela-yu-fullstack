65b4dcd5cd51f792cafd3e5e279ed14c436d4d0b
Cristian Sanchez-Espinosa <csesanchez04@icloud.com>
2025-07-12 14:50:46 -0400

    when the user clicks view button, send them to a new page where the book will be shown in detail

--- capstone projects/project 5/index.js
+++ capstone projects/project 5/index.js
@@ -28,7 +28,7 @@ let currentUserId = 1;
 
 //CRIS/ using the user id, get all the books of the user
 async function getUserBook() {
-    const result = await db.query("SELECT books.title, books.summary, books.book_cover FROM user_book_notes JOIN books ON book_id = books.id WHERE user_id = $1",
+    const result = await db.query("SELECT * FROM user_book_notes JOIN books ON book_id = books.id WHERE user_id = $1",
         [currentUserId]
     );
 
@@ -56,6 +56,17 @@ app.post("/user", async (req, res) => {
 
     res.render("userLibrary.ejs", { name: user, books: books });
 });
+
+//CRIS/ POST /view
+app.post("/view", async (req, res) => {
+    const bookId = req.body["bookId"];
+
+    const result = await db.query("SELECT * FROM books WHERE books.id = $1",
+        [bookId]
+    );
+
+    res.render("userBook.ejs", { book: result.rows[0] });
+})
 
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
