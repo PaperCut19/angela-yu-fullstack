277e90ad6de4d1e30ba81cebca20ddb32e171fe4
Cristian Sanchez-Espinosa <csesanchez04@icloud.com>
2025-08-04 15:08:11 -0400

    add documentation

--- back end/authentication and security/9/index.js
+++ back end/authentication and security/9/index.js
@@ -14,13 +14,14 @@ const app = express();
 const port = 3000;
 const saltRounds = 10;
 
+//CRIS database setup
 const db = new pg.Client({
   user: process.env.PG_USER,
   host: process.env.PG_HOST,
   database: process.env.PG_DATABASE,
   password: process.env.PG_PASSWORD,
   port: process.env.PG_PORT
-})
+});
 db.connect();
 
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -40,18 +41,22 @@ app.use(session({
 app.use(passport.initialize());
 app.use(passport.session());
 
+//CRIS/ GET home page
 app.get("/", (req, res) => {
   res.render("home.ejs");
 });
 
+//CRIS/ GET /login
 app.get("/login", (req, res) => {
   res.render("login.ejs");
 });
 
+//CRIS/ GET /register
 app.get("/register", (req, res) => {
   res.render("register.ejs");
 });
 
+//CRIS/ GET /logout
 app.get("/logout", (req, res) => {
   req.logout(function (err) {
     if (err) {
@@ -61,14 +66,19 @@ app.get("/logout", (req, res) => {
   });
 });
 
+//CRIS/ GET /secrets
 app.get("/secrets", async (req, res) => {
+
+  //CRIS/ if the user is logged in, do the code below
   if (req.isAuthenticated()) {
     console.log(req.user);
 
     try {
+      //CRIS/ get the database row that matches the user's email
       const result = await db.query("SELECT secret FROM users WHERE email = $1", [req.user.email]);
       const secret = result.rows[0].secret;
 
+      //CRIS/ if the user has a secret, then send it to ejs file, if not, then send a specific message
       if (secret) {
         res.render("secrets.ejs", { secret: secret });
       } else {
@@ -78,22 +88,30 @@ app.get("/secrets", async (req, res) => {
       console.log(error);
     }
 
+    //CRIS/ if the user is not authenticated, send them to login page
   } else {
     res.redirect("/login");
   }
 });
 
+//CRIS/ GET /auth/google
+//CRIS/ when user gets sent here, we'll activate the google authentication strategy
 app.get("/auth/google", passport.authenticate("google", {
+  //CRIS/ we want to tell the user that what we want from them is their google profile and email
   scope: ["profile", "email"],
   prompt: "select_account"
 }));
 
+//CRIS/ GET /auth/google/secrets
 app.get("/auth/google/secrets", passport.authenticate("google", {
+  //CRIS/ if the user successfully authenticated with google, send them to /secrets, if not, send them to /login
   successRedirect: "/secrets",
   failureRedirect: "/login"
 }));
 
+//CRIS/ GET /submit
 app.get("/submit", (req, res) => {
+  //CRIS/ if the user is logged in, send them submit ejs file, if not, send them to /login
   if (req.isAuthenticated()) {
     res.render("submit.ejs");
   } else {
@@ -101,44 +119,59 @@ app.get("/submit", (req, res) => {
   }
 });
 
+//CRIS/ POST /submit
 app.post("/submit", async (req, res) => {
+  //CRIS/ store the user's text input in variable called secret
   const secret = req.body.secret;
   console.log(req.user);
 
   try {
+    //CRIS/ in the database, update the user's secret with what they just submitted
     await db.query("UPDATE users SET secret = $1 WHERE email = $2",
       [secret, req.user.email]
     );
 
+    //CRIS/ send the user to /secrets
     res.redirect("/secrets");
   } catch (error) {
     console.log(error);
   }
 });
 
+//CRIS/ POST /register
 app.post("/register", async (req, res) => {
+  //CRIS/ record the user's email input and password input
   const email = req.body["username"];
   const passwordInput = req.body["password"];
 
   try {
+    //CRIS/ check to see if the user exists
     const checkResult = await db.query("SELECT * FROM users WHERE email = $1",
       [email]
     );
 
+    //CRIS/ if they do exist, tell them the email already exists
     if (checkResult.rows.length > 0) {
       res.send("Email already exists. Try logging in");
+
+      //CRIS/ if the user is new, then do the code below
     } else {
+      //CRIS/ hash their password input
       bcrypt.hash(passwordInput, saltRounds, async (error, hash) => {
         if (error) {
           console.error("error hashing password:", error);
         } else {
           console.log("hashed password:", hash);
+          //CRIS/ send the new user's email and hashed password to the database
           const result = await db.query("INSERT INTO users (email, password) VALUES ($1, $2) RETURNING *",
             [email, hash]
           );
+
+          //CRIS/ send the user object to the session storage so that they're logged in
           const user = result.rows[0];
           req.login(user, (error) => {
             console.log(error);
+            //CRIS/ send the user to /secrets
             res.redirect("/secrets");
           });
         }
@@ -149,13 +182,16 @@ app.post("/register", async (req, res) => {
   }
 });
 
+//CRIS/ POST /login
+//CRIS/ use google authentication strategy for user to login
 app.post("/login", passport.authenticate("local", {
+  //CRIS/ if they successfully logged in with google, send them to /secrets, if not, send them to /login
   successRedirect: "/secrets",
   failureRedirect: "/login"
 }));
 
-//CRIS/ set up what authentication strategy passport middleware will use
-//CRIS/ we are creating a new Strategy object and passing in a function which will get stored as a property
+//CRIS/ set up a local authentication strategy with passport middleware
+//CRIS/ we are creating a new Strategy object and passing in a function which will create the configuration and will get stored as a property
 passport.use("local", new Strategy(async function verify(username, password, callback) {
 
   try {
@@ -199,25 +235,33 @@ passport.use("local", new Strategy(async function verify(username, password, cal
 
 }));
 
+//CRIS/ creates the google authentication strategy
 passport.use("google", new GoogleStrategy({
+  //CRIS/ this object is the configuration
   clientID: process.env.GOOGLE_CLIENT_ID,
   clientSecret: process.env.GOOGLE_CLIENT_SECRET,
   callbackURL: "http://localhost:3000/auth/google/secrets",
   userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo"
 }, async (accessToken, refreshToken, profile, callback) => {
+  //CRIS/ this group of code is the callback for what happens after the user successfully logs in with google
   console.log(profile);
+
   try {
+    //CRIS/ get the database row that matches the user's email
     const result = await db.query("SELECT * FROM users WHERE email = $1",
       [profile.email]
     );
 
+    //CRIS/ if there was no row that matches, then the user is new, so we add the new user to the database
     if (result.rows.length === 0) {
       const newUser = await db.query("INSERT INTO users (email, password) VALUES ($1, $2)",
         [profile.email, "google"]
       );
 
+      //CRIS/ after we add the user, we send them to the passport package which will send the user object to the session storage so that they can be logged in
       callback(null, newUser.rows[0]);
     } else {
+      //CRIS/ if the user exists in the database, send them to passport package which sends the user object to session storage which is what logs them in
       callback(null, result.rows[0]);
     }
   } catch (error) {
@@ -231,7 +275,7 @@ passport.serializeUser((user, callback) => {
   callback(null, user);
 });
 
-//CRIS/ this is how our server will take out the user data from the session storage to check it
+//CRIS/ this is how our server will take out the user data from the session storage to check it and to hand off the user object to req.user
 passport.deserializeUser((user, callback) => {
   callback(null, user);
 });
